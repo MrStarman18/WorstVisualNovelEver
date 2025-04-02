e2fab2af3399eb32af5d595e144874b895446cda
Justin T. Alexander <therealjalex38@gmail.com>
2025-04-02 17:02:00 -0500

    Updated framework for new hangout structure
    Day 1 : Story -> 1 hangout
    Day 2: Story -> hangout 1 -> hangout 2
    Day 3: Story -> hangout 1 -> hangout 2
    Ending
    
    Added sSelect dialogue event to centralize the hangout selection in and out of story events.

--- game_state.cs
+++ game_state.cs
@@ -28,23 +28,35 @@ public partial class game_state : Node
 		{
 			int character = (int)((curScene[1]) - '0');	// who were you hanging out with?
 			timesHungout[character]++;
-			curDay++;
-			if (curDay < 3)
-				curScene = $"s{curDay}";
-			else						// 
+			if (curDay > 0 && curHangout < 1)	// choose your second hangout yay!
 			{
-				curScene = "s3";				// default ending cutscene.
-				for (int i = 0; i < timesHungout.Length; i++) {
-					if (timesHungout[i] >= 3) {				// If you hungout with anyone 3 times you get to fuck!!
-						curScene = $"s3{i}";
-						break;
+				curScene = "sSelect";			
+				curHangout++;
+			}
+			else								// on to the next day...
+			{
+				curDay++;		
+				curHangout = 0;		
+				if (curDay < 3)
+				curScene = $"s{curDay}";
+				else						// 
+				{
+					curScene = "s3";				// default ending cutscene.
+					for (int i = 0; i < timesHungout.Length; i++) {
+						if (timesHungout[i] >= 3) {				// If you hungout with anyone 3 times you get to fuck!!
+							curScene = $"s3{i}";
+							break;
+						}
 					}
-				}
+				}		
 			}
 		}
 		else if (curScene[0] == 's')	// if coming from a story event
 		{
-			curScene = $"h{toChar}{timesHungout[toChar]}";
+			if (timesHungout[toChar] < 3)
+				curScene = $"h{toChar}{timesHungout[toChar]}";
+			else
+				curScene = $"h{toChar}{3}";
 		}
 		GetTree().ChangeSceneToFile("res://scenes/cutscene.tscn");
 	}
